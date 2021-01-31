430789519c7356a27ee09e3bdca797ce5906c635
Jooui <jrevertvila@gmail.com>
2021-01-31 16:30:03 +0100

    make build

--- .github/actions/email_notification/dist/index.js
+++ .github/actions/email_notification/dist/index.js
@@ -20,7 +20,7 @@ var mailOptions = {
   from: 'jrevertvila.testing@gmail.com',
   to: core.getInput('MY_EMAIL'),
   subject: 'Resultado del workflow ejecutado',
-  text: 'Se ha realizado un push en la rama githubActions_improvement que ha provocado la ejecución del workflow Bingo_Workflow con los siguientes resultados:\n\n- syntax_check_job: JOB 1\n\n- test_execution_job: JOB 2\n\n - build_statics_job: JOB 3\n\n - deploy_job: JOB 4'
+  text: `Se ha realizado un push en la rama githubActions_improvement que ha provocado la ejecución del workflow Bingo_Workflow con los siguientes resultados:\n\n- syntax_check_job: ${core.getInput('JOB1')}\n\n- test_execution_job: ${core.getInput('JOB2')}\n\n - build_statics_job: ${core.getInput('JOB3')}\n\n - deploy_job: ${core.getInput('JOB4')}`
 };
 
 transporter.sendMail(mailOptions, function(error, info){
